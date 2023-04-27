10182e22b6c0244449d57da8e2e99fabd77e54ad
mrclench <charlesnnnortey@gmail.com>
2023-04-27 14:51:29 +0000

    edit

--- _printf.c
+++ _printf.c
@@ -4,20 +4,44 @@
 
 /**
  * _printf - prints char , int etc 
- *
+ * @format: pointer to string that contains the format specifier
+ * Return: number of characters to be printed
  */
 
 int _printf(const char *format, ...)
 {
+	int count  = 0, i;
+	int (*c)(va_list);
 	va_list args;
 	va_start(args, format);
 
-	int count  = 0, i;
+
 	if (format[0] == '%' && format[i] == '\0')
 		return (-1);
 	while (format != NULL && format[i] != '\0')
 	{
-		if (format[i] = '%')
+		if (format[i + 1] == '%')
 		{
+			count += _putchar(format[i]);
+			i += 2;
+		}
+		else 
+		{
+			c = get_func(format[i + 1]);
+			if (c)
+				count += c(args);
+			else
+				count = _putchar(format[i]) + _putchar(format[i + 1]);
+			i += 2;
+		}
+		else
+		{
+			count += _putchar(format[i]);
+			i++;
+		}
+	}
+	va_end(args);
+	return (count);
+}
 		
 
