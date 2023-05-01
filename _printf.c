342f9a4a6704841b25b0f717f6cc90ad6019a268
mrclench <charlesnnnortey@gmail.com>
2023-05-01 17:54:35 +0000

    check

--- _printf.c
+++ _printf.c
@@ -16,23 +16,28 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
-	if (format[0] == '%' && format[i] == '\0')
+
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	while (format != NULL && format[i] != '\0')
+
+	while (format[i] != '\0')
 	{
-		if (format[i + 1] == '%')
+		if (format[i] == '%')
 		{
-			count += _putchar(format[i]);
-			i += 2;
-		}
-		else
-		{
-			f = get_func(format[i + 1]);
-			if (f)
-				count += c(args);
+			if (format[i + 1] == '%')
+			{
+				count += _putchar('%');
+				i += 2;
+			}
 			else
-				count = _putchar(format[i]) + _putchar(format[i + 1]);
-			i += 2;
+			{
+				f = get_func(format[i + 1]);
+				if (f)
+					count += f(args);
+				else
+					count = _putchar('&') + _putchar(format[i + 1]);
+				i += 2;
+			}
 		}
 		else
 		{
